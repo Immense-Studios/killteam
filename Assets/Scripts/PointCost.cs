de93d1dde9b1f5599043e2ebada47d3de1949dfd
Lucas James <lucas.m.james92@gmail.com>
2019-08-30 00:41:33 +0100

    Added ultramarine and unit script

---
+++ Assets/Scripts/PointCost.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointCost: MonoBehaviour {
+  public int Points;
+
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+
+	public int WeaponSkill;
+	public int BallisticSkill;
+	public int Strength;
+	public int Toughness;
+	public int Wounds;
+	public int Initiative;
+	public int Attacks;
+	public int Leadership;
+	public int ArmorSave;
+}
